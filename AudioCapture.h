c613809427308a723cdb1e1102541d2ef9939d3f
krolackz <krolackz@msoe.edu>
2016-05-19 13:44:40 -0500

    rechanged audio files

--- AudioCapture.h
+++ AudioCapture.h
@@ -1,20 +1,22 @@
-#ifndef AUDIOCAPTURE_H
-#define AUDIOCAPTURE_H
-
-void* tmp(void* a);
-
-class AudioCapture
-{
-public:
-    void captureAudio();
-    void sendAudio();
-    AudioCapture(char* audio);
-
-private:
-    AudioInterface *ai;
-    int rc;
-    char* buffer;
-    int bufferSize;
-};
-
-#endif
++#ifndef AUDIOCAPTURE_H
++#define AUDIOCAPTURE_H
++
++void* tmp(void* a);
++
++class AudioCapture
++{
++public:
++    void captureAudio();
++    void sendAudio();
++    AudioCapture(char* audio);
++
++
++private:
++    AudioInterface *ai;
++    int rc;
++    char* buffer;
++    int bufferSize;
++    int newsockfd;
++};
++
++#endif
