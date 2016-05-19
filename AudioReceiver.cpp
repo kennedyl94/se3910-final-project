a68930ec49e56c1b134d37551f2052bc3bcaca17
krolackz <krolackz@msoe.edu>
2016-05-18 20:38:30 -0500

    commented out main from AudioReceiver and AudioCapture since it was only for demo purposes

--- AudioReceiver.cpp
+++ AudioReceiver.cpp
@@ -13,54 +13,55 @@ using namespace std;
 AudioReceiver::AudioReceiver(char* audio, char* addr)
 {
     int portno, n;
-       struct sockaddr_in serv_addr;
-       struct hostent *server;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
 
-       char buffer[256];
-       if (argc < 3) {
-          fprintf(stderr,"usage %s hostname port\n", argv[0]);
-          exit(0);
-       }
+    char buffer[256];
+    
+	/*if (argc < 3) {
+		fprintf(stderr,"usage %s hostname port\n", argv[0]);
+        exit(0);
+    }*/
 
-       // Read the port number from the command line.
-       portno = atoi(argv[2]);
+    // Hard coded port number for now
+    portno = 5000;
 
-       // Open a socket. See man socket for details.
-       sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    // Open a socket. See man socket for details.
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
-       // If the socket is less than 0, there is an error opening the socket.
-       // Otherwise, the socket has been opened.
-       if (sockfd < 0)
-       {
-           error("ERROR opening socket");
-       }
+    // If the socket is less than 0, there is an error opening the socket.
+    // Otherwise, the socket has been opened.
+    if (sockfd < 0)
+    {
+		error("ERROR opening socket");
+    }
 
-       // Get the hostname.
-       server = gethostbyname(addr);
+    // Get the hostname.
+    server = gethostbyname(addr);
 
-       // If the server is NULL, there is no such host.
-       if (server == NULL) {
-           fprintf(stderr,"ERROR, no such host\n");
-           exit(0);
-       }
+    // If the server is NULL, there is no such host.
+    if (server == NULL) {
+		fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
 
-       // Set the values in the buffer to zero.
-       memset((void*)&serv_addr, 0, sizeof(serv_addr));
+    // Set the values in the buffer to zero.
+    memset((void*)&serv_addr, 0, sizeof(serv_addr));
 
-       // Set up the server address type.
-       serv_addr.sin_family = AF_INET;
+    // Set up the server address type.
+    serv_addr.sin_family = AF_INET;
 
-       // Copy the data.
-       memcpy((void*)&serv_addr.sin_addr.s_addr,(void*)server->h_addr, server->h_length);
+    // Copy the data.
+    memcpy((void*)&serv_addr.sin_addr.s_addr,(void*)server->h_addr, server->h_length);
 
-       // Convert the byte ordering from the host byte ordering to the network byte ordering.
-       serv_addr.sin_port = htons(portno);
+    // Convert the byte ordering from the host byte ordering to the network byte ordering.
+    serv_addr.sin_port = htons(portno);
 
-       // connect to the socket.
-       if (connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
-       {
-           error("ERROR connecting");
-       } //end socket stuff
+    // connect to the socket.
+    if (connect(sockfd,(struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0)
+    {
+		error("ERROR connecting");
+    } //end socket stuff
 
 
     cout << "AudioReceiver created" << endl;
@@ -98,7 +99,7 @@ void AudioReceiver::receiveAudio()
     rc = 1;
 
     do {
-        // Read from the file.
+        // Read from the buffer.
         rc = read(sockfd, buffer, bufferSize);
 
         if (rc > 0) {
@@ -108,6 +109,7 @@ void AudioReceiver::receiveAudio()
     } while (rc > -1);
 }
 
+/*
 int main(int argc, char* argv[])
 {
     if(argc != 2){
@@ -117,3 +119,4 @@ int main(int argc, char* argv[])
     AudioReceiver ar(argv[1]);
     return 0;
 }
+*/
